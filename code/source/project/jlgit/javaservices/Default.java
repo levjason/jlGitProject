bec36267c99b0daab143f0247bedecb4060c0931
ldsgreenadmin <ldsgreenadmin>
2024-04-11 13:50:56 +0000

    Commit made by:ldsgreenadmin

---
+++ code/source/project/jlgit/javaservices/Default.java
@@ -0,0 +1,38 @@
+package project.jlgit.javaservices;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class Default
+
+{
+	// ---( internal utility methods )---
+
+	final static Default _instance = new Default();
+
+	static Default _newInstance() { return new Default(); }
+
+	static Default _cast(Object o) { return (Default)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void unique_name123 (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(unique_name123)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
